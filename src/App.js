21d7d4248e1a052654c9696f955fb0999503cbc6
fredlc <fredlefevre@outlook.com>
2020-12-18 13:32:48 -0500

    Implemented initial state

--- src/App.js
+++ src/App.js
@@ -7,6 +7,11 @@ class App extends React.Component {
     region: "",
     timezone: "",
     hour: "",
+    dayOfWeek: "",
+    weekOfYear: "",
+    date: "",
+    quote: "",
+    quoteAuthor: "",
   };
 
   getUserIp = () => {
@@ -26,18 +31,28 @@ class App extends React.Component {
     const apiUrl = `https://timezoneapi.io/api/ip/?token=${process.env.REACT_APP_API_KEY}`;
     fetch(apiUrl)
       .then((res) => res.json())
-      .then((data) =>
+      .then((data) => {
+        const timeData = data.data.datetime;
         this.setState({
-          hour: data.data.datetime.time.slice(0, 5),
-        })
-      );
+          hour: timeData.time.slice(0, 5),
+          dayOfWeek: timeData.day_full,
+          weekOfYear: timeData.week,
+          week: timeData.week,
+          date: `${timeData.month_full} ${timeData.day} ${timeData.year}`,
+        });
+      });
   };
 
   getRandomQuote = () => {
     const apiUrl = "http://quotes.stormconsultancy.co.uk/random.json";
     fetch(apiUrl)
       .then((res) => res.json())
-      .then((data) => console.log(data));
+      .then((data) =>
+        this.setState({
+          quote: data.quote,
+          quoteAuthor: data.author,
+        })
+      );
   };
 
   componentDidMount() {
@@ -47,7 +62,17 @@ class App extends React.Component {
   }
 
   render() {
-    const { city, region, timezone, hour } = this.state;
+    const {
+      city,
+      region,
+      timezone,
+      hour,
+      dayOfWeek,
+      week,
+      date,
+      quote,
+      quoteAuthor,
+    } = this.state;
     return (
       <div className="App">
         <div>
@@ -57,6 +82,13 @@ class App extends React.Component {
         </div>
         <div>
           <p>{hour}</p>
+          <p>{dayOfWeek}</p>
+          <p>{week}</p>
+          <p>{date}</p>
+        </div>
+        <div>
+          <p>{quote}</p>
+          <p>{quoteAuthor}</p>
         </div>
       </div>
     );
