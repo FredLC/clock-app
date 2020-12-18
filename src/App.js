50d1e0fea716ce94c9761d51f2fdd124b6d813d9
fredlc <fredlefevre@outlook.com>
2020-12-18 13:07:28 -0500

    Initial setup and apis integration

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    city: "",
+    region: "",
+    timezone: "",
+    hour: "",
+  };
+
+  getUserIp = () => {
+    const apiUrl = "https://freegeoip.app/json/";
+    fetch(apiUrl)
+      .then((res) => res.json())
+      .then((data) =>
+        this.setState({
+          city: data.city,
+          region: data.region_code,
+          timezone: data.time_zone,
+        })
+      );
+  };
+
+  getTimezoneData = () => {
+    const apiUrl = `https://timezoneapi.io/api/ip/?token=${process.env.REACT_APP_API_KEY}`;
+    fetch(apiUrl)
+      .then((res) => res.json())
+      .then((data) =>
+        this.setState({
+          hour: data.data.datetime.time.slice(0, 5),
+        })
+      );
+  };
+
+  getRandomQuote = () => {
+    const apiUrl = "http://quotes.stormconsultancy.co.uk/random.json";
+    fetch(apiUrl)
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  };
+
+  componentDidMount() {
+    this.getUserIp();
+    this.getTimezoneData();
+    this.getRandomQuote();
+  }
+
+  render() {
+    const { city, region, timezone, hour } = this.state;
+    return (
+      <div className="App">
+        <div>
+          <p>{city}</p>
+          <p>{region}</p>
+          <p>{timezone}</p>
+        </div>
+        <div>
+          <p>{hour}</p>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
